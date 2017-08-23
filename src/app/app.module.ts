f1285918a90c478a922b242da0a406f790f9ab78
Marcos Guedes <marcosguedes01@gmail.com>
2017-08-23 09:29:05 -0300

    arquivos iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { MenuPage } from '../pages/menu/menu';
+import { RequestPage } from '../pages/request/request';
+import { ProfilePage } from '../pages/profile/profile';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    MenuPage,
+    RequestPage,
+    ProfilePage,    
     TabsPage
   ],
   imports: [
@@ -26,9 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    MenuPage,
+    RequestPage,
+    ProfilePage,    
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { MenuPage } from '../menu/menu';
+import { RequestPage } from '../request/request';
+import { ProfilePage } from '../profile/profile';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MenuPage;
+  tab2Root = RequestPage;
+  tab3Root = ProfilePage;
 
   constructor() {
 
