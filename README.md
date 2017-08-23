b9e4a1056c39872e74a070e89775f9e00cdeec10
Marcos Guedes <marcosguedes01@gmail.com>
2017-08-23 12:12:11 -0300

    Tela de cardápio basica concluída.

--- README.md
+++ README.md
@@ -1,26 +1,15 @@
-This is a starter template for [Ionic](http://ionicframework.com/docs/) projects.
+# Template para aplicativos de Restaurante
 
-## How to use this template
+Modelo para desenvolvimento de aplicativos de restaurante utilizando as seguintes tecnologias:
+- IONIC 3
+- NodeJS 6
 
-*This template does not work on its own*. The shared files for each starter are found in the [ionic2-app-base repo](https://github.com/ionic-team/ionic2-app-base).
+## Iniciando
 
-To use this template, either create a new ionic project using the ionic node.js utility, or copy the files from this repository into the [Starter App Base](https://github.com/ionic-team/ionic2-app-base).
+Para iniciar o projeto execute os seguintes comandos:
+- cd {diretorio_do_app}
+- ionic serve
 
-### With the Ionic CLI:
+## Escopo
 
-Take the name after `ionic2-starter-`, and that is the name of the template to be used when using the `ionic start` command below:
-
-```bash
-$ sudo npm install -g ionic cordova
-$ ionic start myTabs tabs
-```
-
-Then, to run it, cd into `myTabs` and run:
-
-```bash
-$ ionic cordova platform add ios
-$ ionic cordova run ios
-```
-
-Substitute ios for android if not on a Mac.
-
+- Esta primeira versão contempla algumas telas, ainda em desenvolvimento, para seguir com um app de restaurante.
--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the MenuPage page.
@@ -15,11 +15,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MenuPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuPage');
+  }
+
+  showCheckbox() {
+
+    let alert = this.alertCtrl.create();
+    alert.setTitle('O que você deseja filtrar?');
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Almoços',
+      value: 'almocos',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Petiscos',
+      value: 'petiscos'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Lanches',
+      value: 'lanches'
+    });
+
+    alert.addButton('CANCELAR');
+    alert.addButton({
+      text: 'APLICAR',
+      handler: data => {
+        console.log('Checkbox data:', data);
+        //console.log('Checkbox data:', data[0]);
+        //console.log('Checkbox data:', data[0]=="almocos");
+        //this.testCheckboxOpen = false;
+        //this.testCheckboxResult = data;
+      }
+    });
+    alert.present();
   }
 
 }
